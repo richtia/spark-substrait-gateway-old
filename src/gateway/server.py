89583fb241a4ac9a4cc084a9d40cde96b72b9068
Richard Tia <richie.tia@gmail.com>
2024-05-02 09:16:12 -0700

    fix: ruff

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -10,14 +10,13 @@ import pyarrow as pa
 import pyspark.sql.connect.proto.base_pb2 as pb2
 import pyspark.sql.connect.proto.base_pb2_grpc as pb2_grpc
 from pyspark.sql.connect.proto import types_pb2
+from substrait.gen.proto import algebra_pb2
 
 from gateway.backends.backend_options import BackendOptions
 from gateway.backends.backend_selector import find_backend
 from gateway.converter.conversion_options import arrow, datafusion, duck_db
 from gateway.converter.spark_to_substrait import SparkSubstraitConverter
 from gateway.converter.sql_to_substrait import convert_sql
-
-from substrait.gen.proto import algebra_pb2
 
 _LOGGER = logging.getLogger(__name__)
 
